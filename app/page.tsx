53a3e048ee06c0d9ea968cb696adb3a0f7607bbe
pegasis470 <www.sumantdhere@gmail.com>
2025-01-05 16:32:43 +0530

    url fix

--- app/page.tsx
+++ app/page.tsx
@@ -216,7 +216,7 @@ export default function AdoptAnimalPage() {
                             <button
                                 className="adopt-button"
                                 onClick={() =>
-                                    (window.location.href = `/paltu/adoptionform?tag_id=${animalDetails.tag_id}`)
+                                    (window.location.href = `/adoptionform?tag_id=${animalDetails.tag_id}`)
                                 }
                             >
                                 Adopt now
